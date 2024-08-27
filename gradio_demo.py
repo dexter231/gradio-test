f7e57d54e69a7fde1e44dc499f8feb274f01bd6c
dexter231 <90749199+dexter231@users.noreply.github.com>
2024-08-27 17:21:43 +0530

    Update gradio_demo.py

--- gradio_demo.py
+++ gradio_demo.py
@@ -1,5 +1,6 @@
 import gradio as gr
 import time
+import sys
 
 def greet(name):
     return "Hello " + name + "!"
@@ -9,5 +10,6 @@ demo = gr.Interface(fn=greet, inputs="textbox", outputs="textbox")
 local_url, share_url = demo.launch(share=True)
 
 print("Share URL:", share_url)
+sys.stdout.flush()  # Force output buffer to flush
 
 time.sleep(21600)
